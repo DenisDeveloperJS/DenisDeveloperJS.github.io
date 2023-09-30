f4cef5753d4c648b68df330154e4a9df3939aa8e
DenisDeveloperJS <81911115+DenisDeveloperJS@users.noreply.github.com>
2023-09-30 13:04:17 +0300

    Delete src directory

--- src/assets/js/tabs.js
+++
@@ -1,8 +0,0 @@
-import jSuites from "./libs/jsuites-tabs.min.js";
-
-// jSuites.tabs(document.getElementById("process-tabs"), {
-// 	animation: true,
-// 	allowCreate: false,
-// 	allowChangePosition: false,
-// 	padding: "20px",
-// });
