cac0ae1fc1f0aa2143f4416aec036bea70a32af0
DenisDeveloperJS <denisdeveloper4604@gmail.com>
2023-10-01 08:57:03 +0200

    deployed json-server and configured api requests

--- src/redux/actions/games.js
+++ src/redux/actions/games.js
@@ -4,9 +4,13 @@ export const fetchGames = (sortBy, category, order) => dispatch => {
 	dispatch(setLoaded(false));
 	axios
 		.get(
-			`https://denisdeveloperjs.github.io/games?${
-				category === null ? "" : `category=${category}`
-			}&_sort=${sortBy}&_order=${order}`
+			process.env.NODE_ENV === "development"
+				? `http://localhost:3001/games?${
+						category === null ? "" : `category=${category}`
+				  }&_sort=${sortBy}&_order=${order}`
+				: `https://bruiseduncomfortableglobalarrays.denisdevelop460.repl.co/games?${
+						category === null ? "" : `category=${category}`
+				  }&_sort=${sortBy}&_order=${order}`
 		)
 		.then(res => {
 			const data = res.data;
